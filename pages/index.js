9d9f7fa9547404ace302c0c7d99d8fc8bc0a6e71
sandeep-megaska <sandeep.lalu@gmail.com>
2025-12-07 21:08:12 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -44,32 +44,7 @@ export default function Home() {
               flexWrap: "wrap",
             }}
           >
-            {/* Bigonbuy brand */}
            
-            {/* Megaska brand */}
-            <a
-              href="https://www.megaska.com"
-              target="_blank"
-              rel="noreferrer"
-              style={{
-                display: "inline-flex",
-                alignItems: "center",
-                gap: "8px",
-                padding: "10px 14px",
-                borderRadius: "999px",
-                border: "1px solid #ddd",
-                textDecoration: "none",
-                color: "#111",
-                fontSize: "14px",
-              }}
-            >
-              <img
-                src="/logo megaska.png"
-                alt="Megaska"
-                style={{ height: "28px", width: "auto" }}
-              />
-              <span>Megaska</span>
-            </a>
           </div>
         </section>
 
